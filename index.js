199aa7b48448642b80cdd6c587355dbf607e4656
souamya <namankumar62039@gmail.com>
2025-07-01 01:58:23 +0530

    forth commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { urlencoded } from "express";
 import connectDb from "./Db/index.js";
 import dotenv from "dotenv";
 
@@ -6,16 +6,18 @@ dotenv.config({
   path: "./env"
 })
 
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.static());
 
 connectDb()
-const app = express();
 
-app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.send('Hello from Express!');
-});
-
-app.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
-});
+.then(() => {
+  app.listen(process.env.PORT || 8000, () => {
+    console.log(`Server Running at Port : ${process.env.PORT}`);
+  } )
+})
+.catch((error) => {
+console.error(`Error While Connecting ${error}`)
+})
